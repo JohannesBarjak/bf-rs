77bfd31c73f463d1a19b5f839fb93e0571cc07ec
Johannes <johannesbarja@protonmail.com>
2021-03-08 13:48:17 -0600

    Improve brainfuck's loop behavior

--- src/main.rs
+++ src/main.rs
@@ -24,26 +24,39 @@ fn main() {
 
 fn interpreter(input: String, mut tape: Tape) {
     let mut i = 0;
+    let mut lpc = 0;
+
+    let input_char = |i| input.chars().nth(i).unwrap();
 
     while i < input.len() {
-        match input.chars().nth(i).unwrap() {
+        match input_char(i) {
             '+' => tape.cell[tape.ptr] += 1,
             '>' => tape.ptr += 1,
             '<' => tape.ptr -= 1,
 
-            '[' => tape.stack.push(i),
-            ']' => if tape.cell[tape.ptr] != 0 { i = *tape.stack.last().unwrap(); }
-                   else { tape.stack.pop().unwrap(); },
+            '[' => if tape.cell[tape.ptr] != 0 { tape.stack.push(i); } else {
+                       lpc += 1;
+                       while lpc != 0 {
+                           i += 1;
+                           if input_char(i) == '[' { lpc += 1; }
+                           else if input_char(i) == ']' { lpc -= 1; }
+                       }
+                   },
+
+            ']' => if tape.cell[tape.ptr] != 0 { i = *tape.stack.last().unwrap(); } else {
+                       tape.stack.pop().unwrap();
+                   },
 
             '-' => tape.cell[tape.ptr] -= 1,
 
             '.' => { print!("{}", tape.cell[tape.ptr] as char);
-                std::io::stdout().flush().unwrap() },
+                     std::io::stdout().flush().unwrap() },
 
             ',' => tape.cell[tape.ptr] = {
-                let pg_input = std::io::stdin()
-                    .bytes().next().unwrap();
-                pg_input.unwrap() as u8 },
+                       let pg_input = std::io::stdin()
+                           .bytes().next().unwrap();
+                       pg_input.unwrap() as u8
+                   },
 
             _ => ()
         }
