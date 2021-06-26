4d68fe76c6855e5a1193e15de2025ff5ff10b298
johannes <johannesbarja@protonmail.com>
2021-06-26 14:47:53 -0500

    Update version

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -5,7 +5,7 @@ pub fn process_args(input: &str) {
         show_help();
         process::exit(0);
     } else if input == "-v" || input == "--version" {
-        println!("bf-rs v0.3.0");
+        println!("bf-rs v0.4.0");
         process::exit(0);
     }
 }
