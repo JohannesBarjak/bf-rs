89e6845a889ddeab168e3155d1f9d0ac2fc6b7fc
johannes <johannesbarja@protonmail.com>
2021-07-13 22:42:40 -0500

    Update version

--- src/clap.rs
+++ src/clap.rs
@@ -2,7 +2,7 @@ use clap::{App, Arg};
 
 pub fn get_file() -> String {
     let matches = App::new("bf")
-        .version("0.6.1")
+        .version("0.6.3")
         .author("Johannes B. <johannesbarja@protonmail.com>")
         .about("A brainfuck interpreter")
         .arg(
