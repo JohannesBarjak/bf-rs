eaa21dc741191020c2ba82835a68a85203ec887b
johannes <johannesbarja@protonmail.com>
2021-07-15 23:39:59 -0500

    Improve loop logic

--- src/parser.rs
+++ src/parser.rs
@@ -1,106 +1,81 @@
-use std::mem::discriminant;
-
 mod optimizer;
 
 use crate::instructions::Opcode;
 
 pub fn parse(input: String) -> Vec<Opcode> {
-    let input = sanitize(input).as_bytes().to_vec();
     let mut output = Vec::new();
 
-    let mut i = 0;
+    {
+        let input = remove_comments(input).as_bytes().to_vec();
+        let mut i = 0;
 
-    while i < input.len() {
-        for c in b"+-><" {
-            if input[i] == *c {
-                let start = i;
+        while i < input.len() {
+            for c in b"+-><" {
+                if input[i] == *c {
+                    let start = i;
 
-                while i < input.len() && input[i] == *c {
-                    i += 1;
+                    while i < input.len() && input[i] == *c {
+                        i += 1;
+                    }
+
+                    let size = i - start;
+
+                    match *c {
+                        b'+' => output.push(Opcode::Add(size as u8)),
+                        b'-' => output.push(Opcode::Substract(size as u8)),
+                        b'>' => output.push(Opcode::MovePtrRight(size)),
+                        b'<' => output.push(Opcode::MovePtrLeft(size)),
+                        _ => unreachable!(),
+                    }
+
+                    i -= 1;
                 }
-
-                let size = i - start;
-
-                match *c {
-                    b'+' => output.push(Opcode::Add(size as u8)),
-                    b'-' => output.push(Opcode::Substract(size as u8)),
-                    b'>' => output.push(Opcode::MovePtrRight(size)),
-                    b'<' => output.push(Opcode::MovePtrLeft(size)),
-                    _ => unreachable!(),
-                }
-
-                i -= 1;
             }
-        }
 
-        match input[i] {
-            b'[' => output.push(Opcode::LoopStartPlaceholder),
-            b']' => output.push(Opcode::LoopEndPlaceholder),
-            b'.' => output.push(Opcode::PrintChar),
-            b',' => output.push(Opcode::ReadChar),
-            _ => (),
-        }
+            match input[i] {
+                b'[' => output.push(Opcode::LoopStartPlaceholder),
+                b']' => output.push(Opcode::LoopEndPlaceholder),
+                b'.' => output.push(Opcode::PrintChar),
+                b',' => output.push(Opcode::ReadChar),
+                _ => (),
+            }
 
-        i += 1;
+            i += 1;
+        }
     }
 
     optimizer::optimize(&mut output);
 
-    let mut i = 0;
+    {
+        let mut stack = Vec::new();
+        let mut i = 0;
 
-    while i < output.len() {
-        if is_loop_start(&output, &i) || is_loop_end(&output, &i) {
-            let mut loop_count = 1;
-            let start = i;
+        while i < output.len() {
+            match output[i] {
+                Opcode::LoopStartPlaceholder => stack.push(i),
+                Opcode::LoopEndPlaceholder => {
+                    let loop_start = stack.pop().unwrap();
 
-            if is_loop_start(&output, &i) {
-                while loop_count != 0 {
-                    i += 1;
-                    if is_loop_start(&output, &i) {
-                        loop_count += 1;
-                    } else if is_loop_end(&output, &i) {
-                        loop_count -= 1;
-                    }
+                    output[i] = Opcode::LoopEnd {
+                        loop_start_addr: loop_start,
+                    };
+
+                    output[loop_start] = Opcode::LoopStart { loop_end_addr: i };
                 }
-            } else if is_loop_end(&output, &i) {
-                while loop_count != 0 {
-                    i -= 1;
-                    if is_loop_start(&output, &i) {
-                        loop_count -= 1
-                    } else if is_loop_end(&output, &i) {
-                        loop_count += 1;
-                    }
-                }
+
+                _ => (),
             }
 
-            if is_loop_start(&output, &start) {
-                output[start] = Opcode::LoopStart { loop_end_addr: i };
-            } else if is_loop_end(&output, &start) {
-                output[start] = Opcode::LoopEnd { loop_start_addr: i };
-            }
-
-            i = start;
+            i += 1;
         }
-
-        i += 1;
     }
 
     output
 }
 
-fn sanitize(input: String) -> String {
+fn remove_comments(input: String) -> String {
     input
         .chars()
         .filter(|i| "+-><[].,".chars().any(|c| c == *i))
         .collect()
-}
-
-fn is_loop_start(input: &[Opcode], i: &usize) -> bool {
-    input[*i] == Opcode::LoopStartPlaceholder
-        || discriminant(&input[*i]) == discriminant(&Opcode::LoopStart { loop_end_addr: 1 })
-}
-
-fn is_loop_end(input: &[Opcode], i: &usize) -> bool {
-    input[*i] == Opcode::LoopEndPlaceholder
-        || discriminant(&input[*i]) == discriminant(&Opcode::LoopEnd { loop_start_addr: 1 })
 }
--- src/parser/optimizer.rs
+++ src/parser/optimizer.rs
@@ -1,11 +1,10 @@
 use crate::instructions::Opcode;
-use crate::parser::{is_loop_end, is_loop_start};
 
 pub fn optimize(input: &mut Vec<Opcode>) {
     let mut i = 0;
 
     while i < input.len() - 2 {
-        if is_loop_start(&input, &i) && is_loop_end(&input, &(i + 2)) {
+        if input[i] == Opcode::LoopStartPlaceholder && input[i + 2] == Opcode::LoopEndPlaceholder {
             let midpoint = &input[i + 1];
 
             if *midpoint == Opcode::Substract(1) || *midpoint == Opcode::Add(1) {
