553b04babf477bbb629e40bdc7c534d95359b3aa
johannes <johannesbarja@protonmail.com>
2021-09-05 01:33:30 -0500

    Seperate the transpiler header into it's own function

--- src/transpiler.rs
+++ src/transpiler.rs
@@ -3,14 +3,25 @@ use crate::MEMORY_SIZE;
 
 #[must_use]
 pub fn transpile(instructions: Vec<Opcode>) -> String {
-    let mut output = format!(
+    let mut output = c_header();
+
+    transpile_instructions(instructions, &mut output);
+    output.push_str("}\n");
+
+    output
+}
+
+fn c_header() -> String {
+    format!(
         "{}{}{}{}",
         "#include<stdio.h>\n\nint main() {\n",
         format!("    char memory[{}] = {{0}};\n", MEMORY_SIZE),
         "    char *ptr = memory;\n",
         format!("    ptr += {};\n\n", MEMORY_SIZE / 2)
-    );
+    )
+}
 
+fn transpile_instructions(instructions: Vec<Opcode>, output: &mut String) {
     for opcode in instructions {
         match opcode {
             Opcode::Add(n) => output.push_str(format!("    *ptr += {};\n", n as u8).as_str()),
@@ -25,8 +36,4 @@ pub fn transpile(instructions: Vec<Opcode>) -> String {
             Opcode::Clear => output.push_str("    *ptr = 0;\n"),
         }
     }
-
-    output.push_str("}\n");
-
-    output
 }
