e798303db03b3db4a9b4f4568acd12a9de0bc1ac
johannes <johannesbarja@protonmail.com>
2021-09-08 20:23:28 -0500

    It's not necessary to wrap when adding to tape.ptr

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -31,7 +31,7 @@ pub fn interpret(instructions: &[Op], tape: &mut Tape) {
     while i < instructions.len() {
         match &instructions[i] {
             Op::Add(n) => tape.memory[tape.ptr] = tape.memory[tape.ptr].wrapping_add(*n as u8),
-            Op::Move(n) => tape.ptr = tape.ptr.wrapping_add(*n as usize),
+            Op::Move(n) => tape.ptr += *n as usize,
 
             Op::Loop(loop_body) => {
                 while tape.memory[tape.ptr] != 0 {
